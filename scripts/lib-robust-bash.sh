ab272ace28771da33b1bda3dca72b97fd7bd09b4
cladjules <cladjules@gmail.com>
2023-08-01 13:39:39 +0400

    use echo instead of error

--- scripts/lib-robust-bash.sh
+++ scripts/lib-robust-bash.sh
@@ -6,12 +6,12 @@ set -u
 # Check to see that we have a required binary on the path
 function require_binary {
   if [ -z "${1:-}" ]; then
-    error "${FUNCNAME[0]} requires an argument"
+    echo "${FUNCNAME[0]} requires an argument"
     exit 1
   fi
 
   if ! [ -x "$(command -v "$1")" ]; then
-    error "The required executable '$1' is not on the path."
+    echo "The required executable '$1' is not on the path."
     exit 1
   fi
 }
